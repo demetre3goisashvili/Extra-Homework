96b5ab4cbb65d9ec17313854ccc654372e0f309a
demetre3goisashvili <noobdemetre@gmail.com>
2024-03-28 18:02:54 +0400

    second commit

---
+++ day1/project1.py
@@ -0,0 +1,34 @@
+print("Hello customer welcome to the GOA pricing")
+
+print("If you want to have a lesson once in a week u can pay 190 GEL")
+
+print("If you want to have a lesson twice a week pay 290 GEL")
+
+print("If you want to have a lesson three times a week pay 390 GEL")
+
+#price 190 for GOA
+
+price1 = 190
+
+discount = 20
+
+priceAfterDiscount = price1 * discount / 100
+
+#price 290 for GOA
+
+price2 = 290
+price_after_discount = price2 * discount / 100
+
+#price 390 for GOA
+price3 = 390
+price_After_discount = price3 * discount / 100
+
+
+print("Price after discount for 190 GEL")
+print(price1 - priceAfterDiscount)
+
+print("Price after discount for 290 GEL")
+print(price2 - price_after_discount)
+
+print("Price after discount for 390 GEL")
+print(price3 - price_After_discount)

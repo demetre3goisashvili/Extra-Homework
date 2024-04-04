9378a863080fbbdd967031796fb645cc1e40da85
demetre3goisashvili <noobdemetre@gmail.com>
2024-04-04 21:52:30 +0400

    fourth commit

---
+++ day3/project3.py
@@ -0,0 +1,9 @@
+user_input = int(input("please enter the amount of kids that have registered: "))
+Money = 100
+
+if user_input == 10:
+    print(Money * user_input)
+elif user_input == 15:
+    print(Money * user_input)
+elif user_input == 100:
+    print(Money * user_input - 37 + 13 / 10 * 264)
